b4fcbd0984b2127f6fc1f8621d9fe45aaff0c1ce
abdullahazmir <ab.azmir@gmail.com>
2026-02-23 15:38:48 +0600

    toggling work done

---
+++ script/job.js
@@ -0,0 +1,42 @@
+const totalCount = document.getElementById('total-count')
+const interviewCount = document.getElementById('interview-count')
+const rejectedCount = document.getElementById('rejected-count')
+
+// counting-------
+let interviewList = [];
+let rejectedList = [];
+
+// filter button
+
+const allFilterBtn = document.getElementById('all-filter-btn')
+const interviewFilterBtn = document.getElementById('interview-filter-btn')
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn')
+
+// main delegation
+const mainContainer = document.querySelector('main');
+
+console.log(mainContainer)
+
+const allJobs = document.getElementById('all-jobs');
+console.log(allJobs.children.length);
+
+function calculateCount(){
+    totalCount.innerText= allJobs.children.length;
+    interviewCount.innerText =interviewList.length
+    rejectedCount.innerText =rejectedList.length
+}
+calculateCount();
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-blue-600', 'text-white')
+    interviewFilterBtn.classList.remove('bg-blue-600', 'text-white')
+    rejectedFilterBtn.classList.remove('bg-blue-600', 'text-white')
+
+    allFilterBtn.classList.add('bg-gray-300', 'text-black')
+    interviewFilterBtn.classList.add('bg-gray-300', 'text-black')
+    rejectedFilterBtn.classList.add('bg-gray-300', 'text-black');
+
+    const selected = document.getElementById(id);
+    selected.classList.add('bg-blue-600', 'text-white')
+    selected.classList.remove('bg-gray-300', 'text-black')
+}
